d3c8d363bb2f9a5cb7a6e36b6a96daf71b9d3d85
천경훈 <grapduck@gmail.com>
2025-02-13 14:48:49 +0900

    프로젝트 빌드
    구글 AI 스튜디오 샘플

---
+++ app/src/main/kotlin/mrhi3/ai/studio/MainActivity.kt
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mrhi3.ai.studio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import mrhi3.ai.studio.feature.chat.ChatRoute
+import mrhi3.ai.studio.feature.multimodal.PhotoReasoningRoute
+import mrhi3.ai.studio.feature.text.SummarizeRoute
+import mrhi3.ai.studio.ui.theme.GenerativeAISample
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            GenerativeAISample {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+
+                    NavHost(navController = navController, startDestination = "menu") {
+                        composable("menu") {
+                            MenuScreen(onItemClicked = { routeId ->
+                                navController.navigate(routeId)
+                            })
+                        }
+                        composable("summarize") {
+                            SummarizeRoute()
+                        }
+                        composable("photo_reasoning") {
+                            PhotoReasoningRoute()
+                        }
+                        composable("chat") {
+                            ChatRoute()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Preview(showSystemUi = true)
+    @Composable
+    fun SimpleComposablePreview() {
+        GenerativeAISample {
+            // A surface container using the 'background' color from the theme
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = "menu") {
+                    composable("menu") {
+                        MenuScreen(onItemClicked = { routeId ->
+                            navController.navigate(routeId)
+                        })
+                    }
+                    composable("summarize") {
+                        SummarizeRoute()
+                    }
+                    composable("photo_reasoning") {
+                        PhotoReasoningRoute()
+                    }
+                    composable("chat") {
+                        ChatRoute()
+                    }
+                }
+            }
+        }
+    }
+}
+
